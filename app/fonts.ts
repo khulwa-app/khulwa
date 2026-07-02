33c0e02c36d350becdf49708030311d4b81c6cc4
saifmohamedsv <saifmohamed.dev@gmail.com>
2026-07-02 18:23:42 +0300

    feat(fonts): remove IBM Plex Sans Arabic and simplify font variables

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,4 +1,4 @@
-import { IBM_Plex_Sans_Arabic, Nunito } from "next/font/google";
+import { Nunito } from "next/font/google";
 
 export const nunito = Nunito({
   subsets: ["latin"],
@@ -8,12 +8,4 @@ export const nunito = Nunito({
   preload: true,
 });
 
-export const plexSansArabic = IBM_Plex_Sans_Arabic({
-  subsets: ["arabic", "latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-arabic-body",
-  display: "swap",
-  preload: true,
-});
-
-export const fontVariables = `${nunito.variable} ${plexSansArabic.variable}`;
+export const fontVariables = `${nunito.variable}`;
